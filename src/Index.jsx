48441baff379f882923fdf2e0091611822cd4899
Dauvergne Vicente <dauvergne.vicente22@gmail.com>
2024-03-31 11:42:39 -0300

    Parte del sobre mi + img, falta decoracion y propiedades

---
+++ src/Index.jsx
@@ -0,0 +1,22 @@
+import "./Styles/abaout.css";
+
+export function Abaout() {
+  return (
+    <>
+      <h1>
+        Vicente Ignacio <br />
+        Dauvergne
+      </h1>
+      <div className="Me">
+        <div className="Abaout">
+          <h2>Sobre mi</h2>
+          <p>Aqui va la descripcion o la presentacion, incluir tecnologias
+            <br/>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio earum quis possimus nesciunt omnis consequatur velit, consectetur eius amet dolorum beatae pariatur quam autem nihil natus sit aliquid officia? Quod!
+          </p>
+        </div>
+        <img src="../public/assets/me_3.jpg" alt="me"></img>
+      </div>
+    </>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App.jsx";
 import NavBar from "./NavBar.jsx";
-// import './index.css'
+import { Abaout } from "./Index.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <NavBar />
+    <Abaout />
   </React.StrictMode>
 );
