fbb65c22a5e0a2c8829cbceca07b003c7b683099
Dauvergne Vicente <dauvergne.vicente22@gmail.com>
2024-03-31 18:08:50 -0300

    Agregando tecnologias que manejo

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,4 +1,5 @@
 import "./Styles/abaout.css";
+import "./Styles/manejo.css";
 
 export function Abaout() {
   return (
@@ -10,12 +11,39 @@ export function Abaout() {
       <div className="Me">
         <div className="Abaout">
           <h2>Sobre mi</h2>
-          <p>Aqui va la descripcion o la presentacion, incluir tecnologias
-            <br/>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio earum quis possimus nesciunt omnis consequatur velit, consectetur eius amet dolorum beatae pariatur quam autem nihil natus sit aliquid officia? Quod!
+          <p>
+            Aqui va la descripcion o la presentacion, incluir tecnologias
+            <br />
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio earum
+            quis possimus nesciunt omnis consequatur velit, consectetur eius
+            amet dolorum beatae pariatur quam autem nihil natus sit aliquid
+            officia? Quod!
           </p>
         </div>
-        <img src="../public/assets/me_3.jpg" alt="me"></img>
+        <img src="../public/images/me_3.jpg" alt="me"></img>
+      </div>
+    </>
+  );
+}
+
+export function Tecnologias() {
+  return (
+    <>
+      <h2 className="TituloManejo">Lenguajes y tecnologias que manejo</h2>
+      <div className="Manejo">
+        <ul className="Lenguajes">
+          <li>Javascript</li>
+          <li>Python</li>
+          <li>C#</li>
+          <li>SQL</li>
+        </ul>
+        <div>
+          <ul className="Tecnologias">
+            <li>React</li>
+            <li>Django</li>
+            <li>.Net</li>
+          </ul>
+        </div>
       </div>
     </>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import NavBar from "./NavBar.jsx";
-import { Abaout } from "./Index.jsx";
+import { Abaout, Tecnologias } from "./Index.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -9,5 +9,6 @@ root.render(
   <React.StrictMode>
     <NavBar />
     <Abaout />
+    <Tecnologias />
   </React.StrictMode>
 );
